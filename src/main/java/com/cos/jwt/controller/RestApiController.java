070376f9a77ed99b9eec7e0b358f6be20f41c1d6
yyh0126 <yyh0126@naver.com>
2023-08-07 19:08:22 +0900

    Feat : 회원가입

--- src/main/java/com/cos/jwt/controller/RestApiController.java
+++ src/main/java/com/cos/jwt/controller/RestApiController.java
@@ -1,11 +1,20 @@
 package com.cos.jwt.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+//@RequestMapping("api/vi")
+@RequiredArgsConstructor
 public class RestApiController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping ("home")
     public String home() {
@@ -15,5 +24,18 @@ public class RestApiController {
     @PostMapping("token")
     public String token() {
         return "<h1>token</h1>";
+    }
+
+    @GetMapping("admin/users")
+    public List<User> users() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("join")
+    public String join(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+        return "회원가입 완료";
     }
 }
